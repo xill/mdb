ff0c8a96df483af2dd6aab2c314f19ba10110431
xill <koivisto.sami@hotmail.com>
2015-07-10 22:06:55 +0300

    directory view topbar refactor

--- MangaDB/resources/directory/directory.js
+++ MangaDB/resources/directory/directory.js
@@ -81,6 +81,65 @@ function showDirectory() {
 			}
 		}
 	}
+	
+	var menuBtn = $("#menuBtn");
+	menuBtn.bind("click",function(){
+		toggleMenu();
+	});
+	
+	var fsButton = $(".fsButton");
+	var fsIcon = $("#fsIcon");
+	fsButton.bind("click",function(){
+		toggleFullScreen();
+		if (!document.fullscreenElement &&    // alternative standard method
+		!document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
+			// not in fullscreen.
+			fsIcon.attr("src","../images/enterfs.png");
+		}
+		else {
+			fsIcon.attr("src","../images/exitfs.png");
+		}
+	});
+}
+
+function toggleMenu() {
+	var menuDrop = $("#menuDrop");
+	var menuImg = $("#menuImg");
+	var isVisible = menuDrop.is(":visible");
+	if(isVisible) {
+		menuDrop.hide();
+		menuImg.css("transform","");
+	} else {
+		menuDrop.show();
+		menuImg.css("transform","rotate(90deg)");
+	}
+}
+
+function toggleFullScreen( elem ) {
+	
+	if(!elem) elem = document.documentElement;
+	if (!document.fullscreenElement &&    // alternative standard method
+		!document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
+		if (elem.requestFullscreen) {
+			elem.requestFullscreen();
+		} else if (elem.msRequestFullscreen) {
+			elem.msRequestFullscreen();
+		} else if (elem.mozRequestFullScreen) {
+			elem.mozRequestFullScreen();
+		} else if (elem.webkitRequestFullscreen) {
+			elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+		}
+	} else {
+		if (document.exitFullscreen) {
+			document.exitFullscreen();
+		} else if (document.msExitFullscreen) {
+			document.msExitFullscreen();
+		} else if (document.mozCancelFullScreen) {
+			document.mozCancelFullScreen();
+		} else if (document.webkitExitFullscreen) {
+			document.webkitExitFullscreen();
+		}
+	}
 }
 
 $(document).ready(function()
